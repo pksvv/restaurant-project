553ca4cb2d75c167ec680d037d29c79c8938ebb7
Vipul Gaur <gaurvipul@gmail.com>
2020-07-15 09:50:42 +0530

    Created App, links, routes

---
+++ src/components/RestaurantDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RestaurantDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>RestaurantDetails</h1>
+            </div>
+        );
+    }
+}
+
+export default RestaurantDetails;
